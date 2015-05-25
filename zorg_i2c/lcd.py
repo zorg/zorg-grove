dce5acee7c57bb10e5122da78f1d155db5192de0
Gunther Cox <gunthercx@gmail.com>
2015-05-25 19:25:42 -0400

    Updated backlight methods.

--- zorg_i2c/lcd.py
+++ zorg_i2c/lcd.py
@@ -43,17 +43,15 @@ FIVExEIGHTDOTS = 0x00
 BACKLIGHT = 0x08
 NOBACKLIGHT = 0x00
 
-En = 0x04, # Enable bit
-Rw = 0x02, # Read/Write bit
-Rs = 0x01; # Register select bit
+En = 0x04  # Enable bit
+Rw = 0x02  # Read/Write bit
+Rs = 0x01  # Register select bit
 
 
 class LCD(I2CDriver):
 
     def __init__(self, options, connection):
         super(LCD, self).__init__(options, connection)
-
-        self.address = options.get("address", 0x27)
 
         self._backlightVal = NOBACKLIGHT
         self._displayfunction = FOURBITMODE | TWOLINE | FIVExEIGHTDOTS
@@ -67,16 +65,24 @@ class LCD(I2CDriver):
         ]
 
     def start(self):
-        sleep(50)
+
+        # initialise device
+        self.connection.i2c_write(self.address, 0, 0)
+        self.connection.i2c_write(self.address, 1, 0)
+
+        sleep(0.05)
 
         self._expanderWrite(self._backlightVal)
-        sleep(100)
+
+        sleep(0.1)
 
         self._write4bits(0x03 << 4)
-        sleep(4)
+
+        sleep(0.04)
 
         self._write4bits(0x03 << 4)
-        sleep(4)
+
+        sleep(0.04)
 
         self._write4bits(0x03 << 4)
         self._write4bits(0x02 << 4)
@@ -89,70 +95,122 @@ class LCD(I2CDriver):
         self._sendCommand(ENTRYMODESET | self._displaymode)
         self.home()
 
-        '''
-        while True:
-            self._expanderWrite(self._backlightVal)
-        '''
-
     def clear(self):
+        """
+        Clears display and returns cursor to the home position (address 0).
+        """
         self._sendCommand(CLEARDISPLAY)
         sleep(2)
 
     def home(self):
+        """
+        Returns cursor to home position.
+        """
         self._sendCommand(RETURNHOME)
         sleep(2)
 
     def setCursor(self, col, row):
+        """
+        Sets cursor position.
+        """
         row_offsets = [0x00, 0x40, 0x14, 0x54]
         self._sendCommand(SETDDRAMADDR | (col + row_offsets[row]))
 
     def displayOff(self):
+        """
+        Sets Off of all display (D), cursor Off (C) and
+        blink of cursor position character (B).
+        """
         self._displaycontrol &= ~DISPLAYON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def displayOn(self):
+        """
+        Sets On of all display (D), cursor On (C) and
+        blink of cursor position character (B).
+        """
         self._displaycontrol |= DISPLAYON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def cursorOff(self):
+        """
+        Turns off the cursor.
+        """
         self._displaycontrol &= ~CURSORON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def cursorOn(self):
+        """
+        Turns on the cursor.
+        """
         self._displaycontrol |= CURSORON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def blinkOff(self):
+        """
+        Turns off the cursor blinking character.
+        """
         self._displaycontrol &= ~BLINKON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def blinkOn(self):
+        """
+        Turns on the cursor blinking character.
+        """
         self._displaycontrol |= BLINKON
         self._sendCommand(DISPLAYCONTROL | self._displaycontrol)
 
     def backlightOff(self):
+        """
+        Turns off the back light.
+        """
         self._backlightVal = NOBACKLIGHT
         self._expanderWrite(0)
 
     def backlightOn(self):
+        """
+        Turns on the back light.
+        """
         self._backlightVal = BACKLIGHT
         self._expanderWrite(0)
 
+    def backlight_color(self):
+        """
+        Set RGB color for the back light.
+        TODO: Add parameters for values
+        """
+        self.connection.i2c_write(self.address, 0x08, 0xAA)
+        self.connection.i2c_write(self.address, 0x04, 255)
+        self.connection.i2c_write(self.address, 0x02, 255)
+
     def print_string(self, characters):
-        for char in characters:
-            self._writeData(char)
+        """
+        Prints characters on the LCD.
+        """
+        for char in list(characters):
+            self._writeData(ord(char))
 
     def _write4bits(self, val):
         self._expanderWrite(val)
         self._pulseEnable(val)
 
     def _expanderWrite(self, data):
-        self.connection.i2cWrite(self.address, (data | self._backlightVal) & 0xFF)
+
+        print "AAA", data
+
+        x = data | self._backlightVal
+        y = x & 0xFF
+        self.connection.i2c_write(self.address, 0xFF, data)
 
     def _pulseEnable(self, data):
-        self._expanderWrite(data | En)
+
+        print "????????", data, En
+
+        a = data | En
+        self._expanderWrite(data)
         sleep(0.0001)
-        self._expanderWrite(data & ~En)
+        b = data & ~En
+        self._expanderWrite(b)
         sleep(0.05)
 
     def _sendCommand(self, value):
@@ -164,6 +222,8 @@ class LCD(I2CDriver):
     def _sendData(self, val, mode):
         highnib = val & 0xf0
         lownib = (val << 4) & 0xf0
+
+        print "HIGH_LOW", highnib, lownib
 
         self._write4bits(highnib | mode)
         self._write4bits(lownib | mode)
