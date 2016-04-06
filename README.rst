99af0119190bf94897721d6dd0848a699e4453ff
Gunther Cox <gunthercx@gmail.com>
2016-04-05 20:19:04 -0400

    Removed pandoc dependency.

---
+++ README.rst
@@ -0,0 +1,32 @@
+zorg-grove
+==========
+
+|Documentation Status| |Join the chat at
+https://gitter.im/zorg-framework/zorg|
+
+This module implements drivers for controling devices using the
+`Zorg <https://github.com/zorg/zorg>`__ framework for robotics and
+physical computing.
+
+`Documentation <http://zorg-grove.readthedocs.org/>`__
+------------------------------------------------------
+
+Hardware Support
+================
+
+Zorg has a extensible system for connecting to hardware devices. The
+following Grove sensors and devices are currently supported:
+
+-  `LCD screen <docs/LCD.rst>`__
+-  `Temperature sensor <docs/temperature_sensor.rst>`__
+-  `Microphone <docs/microphone.rst>`__
+-  `Rotary Angle Sensor <docs/rotary_angle_sensor.rst>`__
+-  `Servo <docs/servo.rst>`__
+
+`Open a new issue <https://github.com/zorg/zorg-grove/issues/new>`__ to
+request support for additional components.
+
+.. |Documentation Status| image:: https://readthedocs.org/projects/zorg-grove/badge/?version=latest
+   :target: http://zorg-grove.readthedocs.org
+.. |Join the chat at https://gitter.im/zorg-framework/zorg| image:: https://badges.gitter.im/Join%20Chat.svg
+   :target: https://gitter.im/zorg-framework/zorg?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import os
 
-try:
-    from pypandoc import convert
-    readme = lambda f: convert(f, "rst")
-except ImportError:
-    print("Module pypandoc not found, could not convert Markdown to RST")
-    readme = lambda f: open(f, "r").read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-req = open("requirements.txt")
+req = open('requirements.txt')
 requirements = req.readlines()
 req.close()
 
@@ -18,35 +15,35 @@ req.close()
 version_string = __import__('zorg_grove').__version__
 
 setup(
-    name="zorg-grove",
+    name='zorg-grove',
     version=version_string,
-    url="https://github.com/zorg/zorg-grove",
-    description="Python framework for robotics and physical computing.",
-    long_description=readme("readme.md"),
-    author="Zorg Group",
-    author_email="gunthercx@gmail.com",
+    url='https://github.com/zorg/zorg-grove',
+    description='Python framework for robotics and physical computing.',
+    long_description=read('README.rst'),
+    author='Zorg Group',
+    maintainer_email='gunthercx@gmail.com',
     packages=find_packages(),
-    package_dir={"zorg_grove": "zorg_grove"},
+    package_dir={'zorg_grove': 'zorg_grove'},
     include_package_data=True,
     install_requires=requirements,
-    license="MIT",
+    license='MIT',
     zip_safe=True,
-    platforms=["any"],
-    keywords=["zorg", "grove"],
+    platforms=['any'],
+    keywords=['zorg', 'grove'],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Environment :: Console",
-        "Environment :: Web Environment",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
-    test_suite="tests",
-    tests_require=["mock", "six"]
+    test_suite='tests',
+    tests_require=['mock', 'six']
 )
